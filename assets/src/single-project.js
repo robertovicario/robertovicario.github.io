bb1490233075e6621f9cdc63215f8f95fb53ad10
Roberto Vicario <rvicariodev@gmail.com>
2024-07-16 12:34:08 +0200

    updates

--- assets/src/single-project.js
+++ assets/src/single-project.js
@@ -7,14 +7,26 @@ function loadProject(projectName) {
     fetch('data/portfolio.json')
         .then(response => response.json())
         .then(data => {
-            const project = data.portfolio[projectName][0];
-            if (project) {
+            const projects = Object.keys(data.portfolio);
+            const currentIndex = projects.indexOf(projectName);
+
+            if (currentIndex !== -1) {
+                const project = data.portfolio[projectName][0];
                 document.getElementById('name').innerHTML = project.name;
                 document.getElementById('description').innerHTML = project.description;
                 document.getElementById('overview').innerHTML = project.overview;
                 document.getElementById('image').src = project.image;
                 document.getElementById('image-link').href = project.image;
                 document.getElementById('link').href = project.link;
+
+                const prevIndex = currentIndex > 0 ? currentIndex - 1 : projects.length - 1;
+                const nextIndex = currentIndex < projects.length - 1 ? currentIndex + 1 : 0;
+
+                const prevProjectName = projects[prevIndex];
+                const nextProjectName = projects[nextIndex];
+
+                document.getElementById('link-prev').href = `/single-project.html?project=${prevProjectName}`;
+                document.getElementById('link-next').href = `/single-project.html?project=${nextProjectName}`;
             }
         })
         .catch(e => {
