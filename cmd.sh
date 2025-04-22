8967c9d1caea9a4021d003aeedf123cdd4db8c53
robertovicario <robertovicario.com@gmail.com>
2025-04-22 22:30:02 +0200

    🚀 Deploying the app

---
+++ cmd.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+start() {
+    printer "🚀 Starting the app"
+    cd app
+    npm run dev
+    cd ..
+    handler
+}
+
+setup() {
+    printer "🔨 Setting the app"
+    cd app
+    npm install
+    npm run dev
+    cd ..
+    handler
+}
+
+build() {
+    printer "🔨 Setting the app"
+    cd app
+    npm install
+    npm run build
+    cd ..
+    handler
+}
+
+deploy() {
+    printer "🚀 Deploying the app"
+    cd app
+    npm install
+    npm run build
+    cd ..
+    git add .
+    git commit -m "🚀 Deploying the app"
+    git push
+    handler
+}
+
+printer() {
+    echo ""
+    echo "$1"
+    echo ""
+}
+
+handler() {
+    if [ $? -eq 0 ]; then
+        printer "✅ Process completed successfully."
+    else
+        printer "❌ An error occurred during the process."
+        exit 1
+    fi
+}
+
+case "$1" in
+    start)
+        start "$@"
+        ;;
+    setup)
+        setup "$@"
+        ;;
+    build)
+        build "$@"
+        ;;
+    deploy)
+        deploy
+        ;;
+    *)
+        echo "Usage: $0 {start|setup|build|deploy}"
+        ;;
+esac
