a17afbd8e408fd75eb69e8d74daed1595d8e35fe
robertovicario <robertovicario.com@gmail.com>
2025-04-23 18:40:00 +0200

    Deployed the app

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
     integrations: [tailwind(), react()],
