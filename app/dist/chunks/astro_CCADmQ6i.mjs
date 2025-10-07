8b836a24e0d4f6624dcc42efbfec331c602f39a0
robertovicario <robertovicario.com@gmail.com>
2025-10-07 19:30:27 +0200

    Deployed the app

--- app/dist/chunks/astro_CCADmQ6i.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_DPWbZoSU.mjs';
-import 'clsx';
--- app/dist/pages/index.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/index_DRTOi649.mjs';
-export { renderers } from '../renderers.mjs';
